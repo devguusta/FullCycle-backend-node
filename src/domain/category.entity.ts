039092ed7d463d63202f4644d452838bd8de1eec
devguusta <gustavodev60@gmail.com>
2023-12-06 00:16:51 +0000

    feat: create operations category entity

--- src/domain/category.entity.ts
+++ src/domain/category.entity.ts
@@ -22,8 +22,8 @@ export class Category {
     created_at?: Date;
 
 
-    constructor(props: CategoryCOnstructorProps){
-        this.category_id = props.category_id?? null;
+    constructor(props: CategoryCOnstructorProps) {
+        this.category_id = props.category_id ?? null;
         this.name = props.name;
         this.description = props.description ?? null;
         this.is_active = props.is_active ?? true;
@@ -34,7 +34,30 @@ export class Category {
         const category = new Category(props);
         return category;
     }
-    update(props: Partial<CategoryCOnstructorProps>): Category {
-        return Object.assign(this, props);
+
+    changeName(name: string): void {
+        this.name = name;
+    }
+
+    changeDescription(description: string): void {
+        this.description = description;
+    }
+
+    activate(): void {
+        this.is_active = true;
+    }
+
+    deactivate(): void {
+        this.is_active = false;
+    }
+
+    toJSON(): CategoryCOnstructorProps {
+        return {
+            category_id: this.category_id,
+            name: this.name,
+            description: this.description,
+            is_active: this.is_active,
+            created_at: this.created_at
+        }
     }
 }
